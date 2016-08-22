7288a6bd49c8a6a81f7c2cdef8ac48776c293f5e
ChyengJason <654675991@qq.com>
2016-08-22 21:15:32 +0800

    增加首页的tab

--- app/src/main/java/cn/edu/stu/chat/view/activity/MainActivity.java
+++ app/src/main/java/cn/edu/stu/chat/view/activity/MainActivity.java
@@ -1,45 +1,87 @@
 package cn.edu.stu.chat.view.activity;
 
-import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
 
 import cn.edu.stu.chat.R;
+import cn.edu.stu.chat.view.fragment.ContactFragment;
+import cn.edu.stu.chat.view.fragment.MessagingFragment;
+import cn.edu.stu.chat.view.fragment.MineFragment;
 
-public class MainActivity extends AppCompatActivity {
-    private Button toRegister;
-    private Button toLogin;
-    private Button toForgot;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private LinearLayout messagingLayout, contactLayout, mineLayout;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toRegister = (Button)findViewById(R.id.to_register);
-        toLogin =  (Button)findViewById(R.id.to_login);
-        toForgot =  (Button)findViewById(R.id.to_forgot);
-        toRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
-                startActivity(intent);
+        initFragment(savedInstanceState);
+        initView();
+    }
+
+    private void initFragment(Bundle savedInstanceState)
+    {
+        //判断activity是否重建，如果不是，则不需要重新建立fragment.
+        if(savedInstanceState==null) {
+            fragmentManager = getSupportFragmentManager();
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if(fragment==null) {
+                fragment = new MessagingFragment();
             }
-        });
-        toLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-        toForgot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ForgotActivity.class);
-                startActivity(intent);
-            }
-        });
+            ft.replace(R.id.tab_fragment,fragment).commit();
+        }
+    }
+
+    private void initView() {
+        messagingLayout = (LinearLayout) findViewById(R.id.tab_messaging);
+        contactLayout = (LinearLayout) findViewById(R.id.tab_contact);
+        mineLayout = (LinearLayout) findViewById(R.id.tab_mine);
+        messagingLayout.setOnClickListener(this);
+        contactLayout.setOnClickListener(this);
+        mineLayout.setOnClickListener(this);
+        findViewById(R.id.image_messaging).setSelected(true);
+        findViewById(R.id.text_messaging).setSelected(true);
+    }
+
+    @Override
+    public void onClick(View view) {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        initFragmentState();
+        switch (view.getId()){
+            case R.id.tab_messaging:
+                findViewById(R.id.image_messaging).setSelected(true);
+                findViewById(R.id.text_messaging).setSelected(true);
+                fragment = new MessagingFragment();
+                break;
+            case R.id.tab_contact:
+                findViewById(R.id.image_contact).setSelected(true);
+                findViewById(R.id.text_contact).setSelected(true);
+                fragment = new ContactFragment();
+                break;
+            case R.id.tab_mine:
+                findViewById(R.id.image_mine).setSelected(true);
+                findViewById(R.id.text_mine).setSelected(true);
+                fragment = new MineFragment();
+                break;
+        }
+        ft.replace(R.id.tab_fragment, fragment).commit();
+
+    }
+
+    private void initFragmentState() {
+        findViewById(R.id.image_messaging).setSelected(false);
+        findViewById(R.id.text_messaging).setSelected(false);
+        findViewById(R.id.image_contact).setSelected(false);
+        findViewById(R.id.text_contact).setSelected(false);
+        findViewById(R.id.image_mine).setSelected(false);
+        findViewById(R.id.text_mine).setSelected(false);
     }
 }
---
+++ app/src/main/java/cn/edu/stu/chat/view/fragment/MessagingFragment.java
@@ -0,0 +1,30 @@
+package cn.edu.stu.chat.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.edu.stu.chat.R;
+
+/**
+ * Created by cheng on 16-8-22.
+ */
+public class MessagingFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View view = inflater.inflate(R.layout.fragment_contact, null);
+        return view;
+    }
+}
