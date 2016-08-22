a09403a7c2bc4821f51d7932d40832c1f35aee94
water1993 <zlawliex@gmail.com>
2016-08-22 12:22:46 +0800

    add constant

---
+++ app/src/main/java/cn/edu/stu/chat/model/Constant.java
@@ -0,0 +1,18 @@
+package cn.edu.stu.chat.model;
+
+/**
+ * Created by Terence on 2016/8/22.
+ */
+public class Constant {
+    /*
+     *服务器地址
+     */
+    public final static String HOST = "http://test.com";
+
+    /*
+     *登录接口
+     */
+    public final static String LOGIN = HOST + "/test";
+
+
+}
