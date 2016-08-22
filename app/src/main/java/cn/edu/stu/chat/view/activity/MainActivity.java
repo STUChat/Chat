f46d2a2d897b6133280f395339b5c841d6698922
water1993 <zlawliex@gmail.com>
2016-08-22 16:58:10 +0800

    add WelcomeActivity

--- app/src/main/java/cn/edu/stu/chat/view/activity/MainActivity.java
+++ app/src/main/java/cn/edu/stu/chat/view/activity/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         toRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
