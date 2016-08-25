dbcdd11d9e718138794ed61545403de0cb0c862c
water1993 <zlawliex@gmail.com>
2016-08-25 18:13:08 +0800

    inputMethod modify

--- app/src/main/java/cn/edu/stu/chat/view/activity/MessageActivity.java
+++ app/src/main/java/cn/edu/stu/chat/view/activity/MessageActivity.java
@@ -1,12 +1,15 @@
 package cn.edu.stu.chat.view.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import cn.edu.stu.chat.R;
 import cn.edu.stu.chat.adapter.MessageDetailAdapter;
 import cn.edu.stu.chat.model.MessageDetailModel;
@@ -23,16 +26,12 @@ public class MessageActivity extends AppCompatActivity {
         data.setMsg("傻孩子你怎么会是傻孩子!!!");
         data.setName("Terence");
         data.setUserId("lawliex");
-
         datas = new ArrayList<>();
-
         for(int i = 0; i < 100; i ++)
             datas.add(data);
         messageDetailAdapter = new MessageDetailAdapter(this, datas);
         listView = (ListView)findViewById(R.id.message);
         listView.setAdapter(messageDetailAdapter);
-
         listView.setSelection(datas.size() - 1);
-
     }
 }
