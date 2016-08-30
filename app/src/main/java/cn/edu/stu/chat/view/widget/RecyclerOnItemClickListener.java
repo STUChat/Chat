d1c95ac89516e6a32960932c8a746b857312e846
ChyengJason <654675991@qq.com>
2016-08-31 00:34:54 +0800

    返回聊天friend信息，改动很大

---
+++ app/src/main/java/cn/edu/stu/chat/view/widget/RecyclerOnItemClickListener.java
@@ -0,0 +1,11 @@
+package cn.edu.stu.chat.view.widget;
+
+import android.view.View;
+
+/**
+ * Created by dell on 2016/8/30.
+ * recycle item点击事件
+ */
+public interface RecyclerOnItemClickListener {
+    public abstract void onClick(View view);
+}
